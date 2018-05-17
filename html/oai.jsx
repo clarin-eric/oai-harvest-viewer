2fbb8175c0a6c1ee5071ac7899d0819efa4466ba
Menzo Windhouwer <menzo@windhouwer.nl>
2018-05-17 12:37:22 +0200

    modified:   html/oai.jsx - do actually remove the vars which are now in config.js

--- html/oai.jsx
+++ html/oai.jsx
@@ -1,14 +1,3 @@
-// OAI DreamFactory API
-var base = "http://localhost/api/v2/oai/_table/";
-var key = "00551c93af07a0e2c22628ad6214b9ab250cdfa82a5be2fc04789920e27a7170";
-var endPagesize = 10;
-var recPagesize = 1000;
-
-// endpoints
-var curationModule = "https://clarin.oeaw.ac.at/curate/#!ResultView/collection/";
-var logDir         = "http://localhost/oai-harvest-result/log";
-var outDir         = "http://localhost/oai-harvest-result/output";
-
 // react-bootstrap imports
 var PageHeader = ReactBootstrap.PageHeader;
 var Table = ReactBootstrap.Table;
