695d36331af4c9d186f3062228290bb46fe907d7
Menzo Windhouwer <menzo@windhouwer.nl>
2018-05-18 16:26:04 +0200

    modified:   html/config.js - use environment variables

--- html/config.js
+++ html/config.js
@@ -1,5 +1,6 @@
 // OAI DreamFactory API
-var base = "http://localhost/api/v2/oai/_table/";
+//var base = "http://localhost/api/v2/oai/_table/";
+var base = "${DF_API_URI}";
 var key = "00551c93af07a0e2c22628ad6214b9ab250cdfa82a5be2fc04789920e27a7170";
 var endPagesize = 10;
 var recPagesize = 1000;
@@ -7,4 +8,5 @@ var recPagesize = 1000;
 // endpoints
 var curationModule = "https://clarin.oeaw.ac.at/curate/#!ResultView/collection/";
 //var logDir         = "http://localhost/oai-harvest-result/log";
-var outDir         = "http://localhost/oai-harvest-result/output";
+//var outDir         = "http://localhost/oai-harvest-result/output";
+var outDir         = "${OAI_OUTPUT_URI}";
